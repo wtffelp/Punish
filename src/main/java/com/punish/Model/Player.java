c63d626b694c51a242dcde66271cb1e5a2755cac
Felp <wtffelp@gmail.com>
2026-06-15 21:05:00 -0300

    feat: add Player model

---
+++ src/main/java/com/punish/Model/Player.java
@@ -0,0 +1,14 @@
+package com.punish.Model;
+
+public class Player {
+    private Long id;
+    private String nickname;
+
+    public Player () {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNickname() { return nickname; }
+    public void setNickname(String nickname) { this.nickname = nickname; }
+}
