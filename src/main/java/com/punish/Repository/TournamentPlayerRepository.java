32abbeb1fe92c59b40fe5af5550794468acb051a
Felp <wtffelp@gmail.com>
2026-06-19 15:51:36 -0300

    feat: add TournamentPlayerRepository.java

---
+++ src/main/java/com/punish/Repository/TournamentPlayerRepository.java
@@ -0,0 +1,81 @@
+package com.punish.Repository;
+
+import java.util.List;
+
+import org.jdbi.v3.core.Jdbi;
+
+import com.punish.Config.Database;
+import com.punish.Model.TournamentPlayer;
+
+public class TournamentPlayerRepository {
+    //  - criar(long tournamentId, long playerId) → TournamentPlayer
+    //  - deletar(long tournamentId, long playerId) → void
+    //  - buscarPlayersPorTournament(long tournamentId) → List<Player>
+    //  - existe(long tournamentId, long playerId) → boolean
+    //  - contarPorTournament(long tournamentId) → int
+    //  - atualizarSeed(long tournamentId, long playerId, int seed) → void
+
+    Jdbi jdbi = Database.getJdbi();
+    
+    public TournamentPlayer criarTournamentPlayer(Long fk_tournament_id, Long fk_player_id){
+        return jdbi.withHandle(handle -> {
+            return handle.createUpdate("INSERT INTO tournament_player (fk_tournament_id, fk_player_id) VALUES (:fk_tournament_id, fk_player_id)")
+                .bind("fk_tournament_id", fk_tournament_id)
+                .bind("fk_player_id", fk_player_id)
+                .executeAndReturnGeneratedKeys("id")
+                .mapToBean(TournamentPlayer.class)
+                .findOne()
+                .orElse(null);
+        });
+    }
+
+    public void deletarTournamentPlayer(Long fk_tournament_id, Long fk_player_id){
+        jdbi.withHandle(handle -> {
+            return handle.createUpdate("DELETE FROM tournament_player WHERE fk_tournament_id = :fk_tournament_id AND fk_player_id = :fk_player_id")
+            .bind("fk_tournament_id", fk_tournament_id)
+            .bind("fk_player_id", fk_player_id)
+            .execute();
+        });
+    }
+
+    public List<TournamentPlayer> buscarPlayerPorTournament(Long fk_tournament_id){
+        return jdbi.withHandle(handle -> {
+            return handle.createQuery("SELECT * FROM tournament_player WHERE fk_tournament_id = :fk_tournament_id")
+            .bind("fk_tournament_id", fk_tournament_id)
+            .mapToBean(TournamentPlayer.class)
+            .list();
+        });
+    }
+
+    public boolean existe(long fk_tournament_id, long fk_player_id){
+        return jdbi.withHandle(handle -> 
+            handle.createQuery("SELECT COUNT(*) > 0  FROM tournament_player WHERE fk_tournament_id = :tid AND fk_player_id = :pid")
+                .bind("tid", fk_tournament_id)
+                .bind("pid", fk_player_id)
+                .mapTo(Boolean.class)
+                .findOne()
+                .orElse(false)
+        );
+    }
+
+    public int contarPorTournament(long fk_tournament_id){
+        return jdbi.withHandle(handle -> 
+            handle.createQuery("SELECT COUNT(*) FROM tournament_player WHERE fk_tournament_id = :tid")
+            .bind("tid", fk_tournament_id)
+            .mapTo(Integer.class)
+            .findOne()
+            .orElse(0)
+        );
+    }
+
+    public void atualizarSeed(long fk_tournament_id, long fk_player_id, int seed){
+        jdbi.withHandle(handle -> 
+            handle.createUpdate("UPDATE tournament_player SET seed = :seed WHERE fk_tournament_id = :tid AND fk_player_id = :pid")
+            .bind("tid", fk_tournament_id)
+            .bind("pid", fk_player_id)
+            .bind("seed", seed)
+            .execute()
+        );
+    }
+
+}
