bc37367d8133b3883d4c34d5b74713525a02bc6e
Felp <wtffelp@gmail.com>
2026-06-25 14:42:20 -0300

    feat: add App entrypoint with Javalin setup and route registration

--- src/main/java/com/punish/App.java
+++ src/main/java/com/punish/App.java
@@ -1,9 +1,27 @@
 package com.punish;
 
+import com.punish.Controller.MatchController;
+import com.punish.Controller.PlayerController;
+import com.punish.Controller.TournamentController;
+
+import io.javalin.Javalin;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        
+        Javalin javalin = Javalin.create( config -> {
+            config.bundledPlugins.enableCors(cors -> {
+                cors.addRule(rule -> {
+                    // rule.allowHost("https://punish.vercel.app")
+                    rule.anyHost();
+                });
+            });
+        }).start(7000);
+
+        new MatchController().matchRoutes(javalin);
+        new PlayerController().playerRoutes(javalin);
+        new TournamentController().tournamentRoutes(javalin);
+
     }
 }
